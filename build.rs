baffd9c19f5a2bceacf240b8d9726ef2734c2501
frederik <termite_pilgrim04@icloud.com>
2025-07-23 21:47:42 +0200

    .

--- build.rs
+++ build.rs
@@ -255,8 +255,6 @@ fn main() {
             }
         }
         if openblas {
-            println!("cargo:rustc-link-search=native=/opt/OpenBLAS/lib");
-            println!("cargo:include=/home/runner/work/ctranslate2-rs/ctranslate2-rs/OpenBLAS-0.3.21/include");
             println!("cargo:rustc-link-lib=static=openblas");
             cmake.define("WITH_OPENBLAS", "ON");
         }
