b1598f109c0bf0f85e946c2a85a504f5b65dce2c
frederik <termite_pilgrim04@icloud.com>
2025-07-23 15:10:32 +0200

    .

--- build.rs
+++ build.rs
@@ -218,10 +218,13 @@ fn main() {
             cmake.define("WITH_CUDA", "ON");
             cmake.define("CUDA_TOOLKIT_ROOT_DIR", "Common");
             if os == Os::Linux {
-                cmake.define("CMAKE_CUDA_FLAGS", "--include stdint.h");
+                cmake.define("CMAKE_CUDA_FLAGS", "-Xcompiler --include stdint.h");
             }
             if cfg!(feature = "cuda-small-binary") {
-                cmake.define("CUDA_NVCC_FLAGS", "-Xfatbin=-compress-all");
+                cmake.define(
+                    "CUDA_NVCC_FLAGS",
+                    "-Xfatbin=-compress-all -Xcompiler --include stdint.h",
+                );
             }
             println!("cargo:rustc-link-search={}", cuda.join("lib").display());
             println!("cargo:rustc-link-search={}", cuda.join("lib64").display());
