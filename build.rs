0771d79f089626e93f86f1fed847995afe945170
frederik <termite_pilgrim04@icloud.com>
2025-07-24 12:05:24 +0200

    .

--- build.rs
+++ build.rs
@@ -247,13 +247,6 @@ fn main() {
 
         if mkl {
             cmake.define("WITH_MKL", "ON");
-            if os == Os::Win {
-                let mkl_root = env::var("MKLROOT").unwrap_or_else(|_| {
-                    panic!("MKLROOT environment variable not set");
-                });
-                println!("cargo:include={}/include", mkl_root);
-                link_libraries(mkl_root);
-            }
         }
         if openblas {
             println!("cargo:rustc-link-lib=static=openblas");
